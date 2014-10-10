01be36678f417f0bf424f8783c1ad38681ad1c40
Ivan <ivan@softwarecriollo.com>
2014-10-10 11:58:55 +0700

    sample controller

--- In App Purchase Example/ViewController.swift
+++ In App Purchase Example/ViewController.swift
@@ -7,19 +7,103 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    var product_id: NSString?;
+    
     override func viewDidLoad() {
+        product_id = "iap.comsumable";
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addBotton();
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addBotton(){
+        let buyBottom = UIButton(frame: CGRectMake(100, 320, 200, 40))
+        buyBottom.setTitle("Buy Consumable", forState: UIControlState.Normal);
+        buyBottom.backgroundColor = UIColor(red: 0.0, green: 0.2, blue: 0.2, alpha: 1.0)
+        buyBottom.addTarget(self, action: "buyConsumable", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(buyBottom);
+        
     }
+    
+    
+    func buyConsumable(){
+        println("About to fetch the products");
+        // We check that we are allow to make the purchase.
+        if (SKPaymentQueue.canMakePayments())
+        {
+            var productID:NSSet = NSSet(object: self.product_id!);
+            var productsRequest:SKProductsRequest = SKProductsRequest(productIdentifiers: productID);
+            productsRequest.delegate = self;
+            productsRequest.start();
+            println("Fething Products");
+        }else{
+            println("can't make purchases");
+        }
+    }
+    
+    // Helper Methods
+    
+    func buyProduct(product: SKProduct){
+        println("Sending the Payment Request to Apple");
+        var payment = SKPayment(product: product)
+        SKPaymentQueue.defaultQueue().addPayment(payment);
+        
+    }
+    
 
+    // Delegate Methods for IAP
+    
+    func productsRequest (request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+        println("got the request from Apple")
+        var count : Int = response.products.count
+        if (count>0) {
+            var validProducts = response.products
+            var validProduct: SKProduct = response.products[0] as SKProduct
+            if (validProduct.productIdentifier == self.product_id) {
+                println(validProduct.localizedTitle)
+                println(validProduct.localizedDescription)
+                println(validProduct.price)
+                buyProduct(validProduct);
+            } else {
+                println(validProduct.productIdentifier)
+            }
+        } else {
+            println("nothing")
+        }
+    }
+    
+    
+    func request(request: SKRequest!, didFailWithError error: NSError!) {
+        println("La vaina fallo");
+    }
+    
+    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!)    {
+        println("Received Payment Transaction Response from Apple");
+        
+        for transaction:AnyObject in transactions {
+            if let trans:SKPaymentTransaction = transaction as? SKPaymentTransaction{
+                switch trans.transactionState {
+                case .Purchased:
+                    println("Product Purchased");
+                    SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                    break;
+                case .Failed:
+                    println("Purchased Failed");
+                    SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                    break;
+                    // case .Restored:
+                    //[self restoreTransaction:transaction];
+                default:
+                    break;
+                }
+            }
+        }
+        
+    }
 
 }
+
 
